caaa42649d5776e46c9a1525bdf76fbd8553fea5
Ngoc Nguyen Luu <ngoc.nguyenluu@grabtaxi.com>
2024-05-17 12:46:33 +0800

    update GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-87659525-1",
+        trackingId: "G-4V9N2KDY2K",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         respectDNT: true,
